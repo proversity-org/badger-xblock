d543c6236196868bdf444372eb536a1a689beef3
lidijarad <radovanovic.lidija22@gmail.com>
2017-10-03 10:35:01 +0100

    leave badgr api token available in badger_edit js file

--- badger/static/js/src/badger_edit.js
+++ badger/static/js/src/badger_edit.js
@@ -5,11 +5,7 @@ function StudioEditableXBlockMixin(runtime, element, data) {
     var fields = [];
     var tinyMceAvailable = (typeof $.fn.tinymce !== 'undefined'); // Studio includes a copy of tinyMCE and its jQuery plugin
     var datepickerAvailable = (typeof $.fn.datepicker !== 'undefined'); // Studio includes datepicker jQuery plugin
-
     var badgrApiToken = data.badgrApiToken;
-    console.log("hi", data.badgrApiToken);
-
-    $('#dropdownl').append('<option value="unique-issuer">' + 'khda'+ '</option>')
 
     $(element).find('.field-data-control').each(function() {
         var $field = $(this);
